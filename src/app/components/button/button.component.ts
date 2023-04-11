8414632d2ef6b40958121fa1920913e93b82df38
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2023-04-11 19:27:57 +0100

    add header and button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
+  @Input() text!: string;
+  @Input() color!: string;
 
+  ngOnInit(): void {}
+
+  onClick() {
+    console.log('Add');
+  }
 }
