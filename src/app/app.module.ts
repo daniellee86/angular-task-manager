b3edf1026900938ded993828504cf3100caa39f3
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2023-04-12 16:11:06 +0100

    add task item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TaskComponent } from './components/task/task.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent, TaskComponent],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, TaskComponent, TaskItemComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-tasks';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  styleUrls: ['./task.component.css'],
 })
 export class TaskComponent {
-
+  //set tasks as a property of component
+  tasks: Task[] = TASKS;
 }
