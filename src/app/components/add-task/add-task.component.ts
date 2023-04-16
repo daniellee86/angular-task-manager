3147d1d40cd825bf7c29a2fe2f46a3e46f92704e
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2023-04-16 17:52:56 +0100

    add add-task component

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,7 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
   text: string;
   day: string;
   reminder: boolean = false;
+
+  onSubmit() {
+    //SIMPLE FORM VALIDATION
+    if (!this.text) {
+      alert('Please add a task!');
+      return;
+    }
+
+    //NEW TASK OBJECT - DATA BINDING WITH FORM FIELDS
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    //EMIT EVENT WITH NEW TASK OBJECT
+    this.onAddTask.emit(newTask);
+
+    //CLEAR FORM FIELDS
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
